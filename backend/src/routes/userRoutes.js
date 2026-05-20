4776bd0da21dcd7478b173b19d936961d6002d59
Emil Larsen <emilvl@stud.ntnu.no>
2026-05-20 18:01:47 +0200

    implemened validations for creating updating and login user removed select(-password) and putted it in schema so we dont DRY

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,33 +1,29 @@
-import express from 'express';
-import { body, validationResult } from 'express-validator';
-import { requireUser, requireAdmin } from '../middlewares/authMiddleware.js';
-import { getAllUsers, getUser, registerUser, loginUser, updateUser, banUser } from '../controllers/userController.js';
+import express from "express";
+import { body, validationResult } from "express-validator";
+import { requireUser, requireAdmin } from "../middlewares/authMiddleware.js";
+import {
+  validateUser,
+  validateUpdateUser,
+  validateLogin,
+} from "../validators/userValidator.js";
+import {
+  getAllUsers,
+  getUser,
+  registerUser,
+  loginUser,
+  updateUser,
+  banUser,
+} from "../controllers/userController.js";
+import { validate } from "../validators/validate.js";
 
 const userRouter = express.Router();
-// these are the validation rules that will run before the register controller
-const registerRules = [
-  body('username').notEmpty().withMessage('Username is required'),
-  body('email').isEmail().withMessage('Valid email is required'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
-  body('age').isInt({ min: 18 }).withMessage('You must be at least 18 years old'),
-]; // TODO: Move these to its own validation folder
 
-// this will check if all the validation rules passed
-// if there are errors it will send them back and stop the request from reaching the controller
-const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
-}; // TODO: Move to its own validation folder
+userRouter.post("/register", validateUser(), validate, registerUser);
+userRouter.post("/login", validateLogin(), validate, loginUser);
 
-// this will run the validation rules and then the validate check before registering
-userRouter.post('/register', registerRules, validate, registerUser);
-userRouter.post('/login', loginUser);
-userRouter.get('/', requireAdmin, getAllUsers);       // this will block anyone who is not an admin
-userRouter.get('/:id', getUser);
-userRouter.patch('/:id', requireUser, updateUser);   // this will block anonymous users
-userRouter.post('/:id/ban', requireAdmin, banUser);  // this will block anyone who is not an admin
+userRouter.get("/", requireAdmin, getAllUsers); // this will block anyone who is not an admin
+userRouter.get("/:id", validate, getUser);
+userRouter.patch("/:id", requireUser, validateUpdateUser(), updateUser, validate); // this will block anonymous users
+userRouter.post("/:id/ban", requireAdmin, banUser, validate); // this will block anyone who is not an admin
 
-export default userRouter
+export default userRouter;
---
+++ backend/src/validators/userValidator.js
@@ -0,0 +1,85 @@
+import { body } from "express-validator";
+
+import {
+  MIN_USER_USERNAME_LENGTH,
+  MAX_USER_USERNAME_LENGTH,
+  MIN_USER_PASSWORD_LENGTH,
+  MAX_USER_PASSWORD_LENGTH,
+  MIN_USER_AGE_LENGTH,
+  MAX_USER_AGE_LENGTH,
+  MAX_USER_BIO_LENGTH,
+} from "../config/constants.js";
+
+export const validateUser = () => {
+  return [
+    body("username")
+      .trim()
+      .escape()
+      .isAlphanumeric()
+      .withMessage("Username can only contain alpha numeric characters")
+      .isLength({
+        min: MIN_USER_USERNAME_LENGTH,
+        max: MAX_USER_USERNAME_LENGTH,
+      })
+      .withMessage(
+        `Username needs to between ${MIN_USER_USERNAME_LENGTH} and ${MAX_USER_USERNAME_LENGTH} characters long`,
+      ), // TODO: Create a function to check if a username is taken
+    body("password")
+      .trim()
+      .isStrongPassword({
+        min: MIN_USER_PASSWORD_LENGTH,
+        max: MAX_USER_PASSWORD_LENGTH,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumber: 1,
+      })
+      .withMessage(
+        `Password needs to be between ${MIN_USER_PASSWORD_LENGTH} and ${MAX_USER_PASSWORD_LENGTH} characters long`,
+      ),
+    body("email").trim().escape().isEmail().withMessage("Not a valid email"), // TODO: create a function to check if mail is in use
+    body("age")
+      .isInt({
+        min: MIN_USER_AGE_LENGTH,
+        max: MAX_USER_AGE_LENGTH,
+      })
+      .withMessage(
+        `You need to be between the age of ${MIN_USER_AGE_LENGTH} to ${MAX_USER_AGE_LENGTH} to play this game`,
+      )
+      .bail()
+      .toInt(),
+  ];
+};
+
+export const validateUpdateUser = () => {
+  return [
+    body("email")
+      .trim()
+      .optional()
+      .isEmail()
+      .withMessage("Must be a valid email"),
+    body("password")
+      .trim()
+      .optional()
+      .isStrongPassword({
+        min: MIN_USER_PASSWORD_LENGTH,
+        max: MAX_USER_PASSWORD_LENGTH,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumber: 1,
+      })
+      .withMessage(
+        `Password needs to be between ${MIN_USER_PASSWORD_LENGTH} and ${MAX_USER_PASSWORD_LENGTH} characters long`,
+      ),
+  ];
+};
+
+export const validateLogin = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .withMessage("A valid email is required"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+  ];
+};
