4cf9416ae12c8a04ce28d1504ef79d790e71ad5f
posepilt <adfm98@hotmail.no>
2026-05-26 12:40:19 +0200

    Update tournament routes with formatting and validator middleware

--- backend/src/routes/tournament-routes.js
+++ backend/src/routes/tournament-routes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { validateCreateTournament, validateUpdateTournament } from "../validators/tournament-validator.js";
+import {
+  validateGetTournaments,
+  validateCreateTournament,
+  validateUpdateTournament,
+  validateReportMatchResult,
+} from "../validators/tournament-validator.js";
 import { validate } from "../validators/validate.js";
 import upload from "../middlewares/upload.js";
 import {
@@ -19,13 +24,15 @@ import { authenticate, authorize } from "../middlewares/auth-middleware.js";
 
 const tournamentRouter = express.Router();
 
+
 // Public routes
-tournamentRouter.get("/", getAllTournaments);
+tournamentRouter.get("/", validateGetTournaments(), validate, getAllTournaments);
 tournamentRouter.get("/:id", getTournament);
 tournamentRouter.get("/:id/standings", getStandings);
 
 
 tournamentRouter.use(authenticate);
+
 
 // Authenticated users
 tournamentRouter.post("/:id/join", authorize("user"), joinTournament);
@@ -33,11 +40,12 @@ tournamentRouter.post("/:id/leave", authorize("user"), leaveTournament);
 
 
 // Admin only
-tournamentRouter.post("/", authorize("admin"), upload.single("trophyImage"), validateCreateTournament(), validate, createTournament); // create new
-tournamentRouter.put("/:id", authorize("admin"), validateUpdateTournament(), validate, updateTournament); // update tournament
-tournamentRouter.post("/:id/start", authorize("admin"), startTournament); // start tournament
-tournamentRouter.post("/:id/cancel", authorize("admin"), cancelTournament); // cancel tournament
-tournamentRouter.put("/:id/matches/:matchId/result", authorize("admin"), reportMatchResult); // get result on tournament
-tournamentRouter.delete("/:id", authorize("admin"), deleteTournament); // delete tournament
+tournamentRouter.post("/", authorize("admin"), upload.single("trophyImage"), validateCreateTournament(), validate, createTournament);
+tournamentRouter.put("/:id", authorize("admin"), validateUpdateTournament(), validate, updateTournament);
+tournamentRouter.post("/:id/start", authorize("admin"), startTournament);
+tournamentRouter.post("/:id/cancel", authorize("admin"), cancelTournament);
+tournamentRouter.put("/:id/matches/:matchId/result", authorize("admin"), validateReportMatchResult(), validate, reportMatchResult);
+tournamentRouter.delete("/:id", authorize("admin"), deleteTournament);
+
 
 export default tournamentRouter;
