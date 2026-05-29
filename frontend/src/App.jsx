cb445d177398d0154f87e43bd3f3ab4a6633b619
posepilt <adfm98@hotmail.no>
2026-05-29 12:43:04 +0200

    Strip stylesheets of exessive declarations, reorder declarations, and delete /all-games as its not needed anymore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,6 @@ import Register from "./pages/register/Register.jsx";
 import Game from "./pages/game/Game.jsx";
 import CreateGame from "./pages/create-game/CreateGame.jsx";
 import Profile from "./pages/profile/Profile.jsx";
-import AllGames from "./pages/all-games/AllGames.jsx";
 import TournamentList from "./pages/tournament-list/TournamentList.jsx";
 import Tournament from "./pages/tournament/Tournament.jsx";
 import AboutUs from "./pages/about-us/AboutUs.jsx";
@@ -42,7 +41,6 @@ function App() {
               <Route path="/tournaments" element={<TournamentList />} />
               <Route path="/tournament/:id" element={<Tournament />} />
               <Route path="/profile/:id" element={<Profile />} />
-              <Route path="/profile/:id/games" element={<AllGames />} />
               <Route path="/about-us" element={<AboutUs />} />
               <Route path="/about-game" element={<AboutGame />} />
               <Route path="/terms" element={<Terms />} />
