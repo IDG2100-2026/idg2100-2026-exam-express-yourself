0a2fe9467f9e60fda699720be0e4ffebaf8daaca
Emil Larsen <emilvl@stud.ntnu.no>
2026-05-29 13:47:59 +0200

    feature: added forgot password to its own page, and removed it from login

--- backend/src/validators/passwordResetValidator.js
+++ backend/src/validators/passwordResetValidator.js
@@ -1,5 +1,8 @@
 import { body } from "express-validator";
-import { MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH } from "../config/constants.js";
+import {
+  MIN_PASSWORD_LENGTH,
+  MAX_PASSWORD_LENGTH,
+} from "../config/constants.js";
 
 export const validatePasswordReset = () => {
   return [
@@ -11,7 +14,7 @@ export const validatePasswordReset = () => {
         minLowercase: 1,
         minUppercase: 1,
         minNumbers: 1,
-        minSymbols: 1
+        minSymbols: 1,
       })
       .withMessage(
         `New password needs to be between ${MIN_PASSWORD_LENGTH} and ${MAX_PASSWORD_LENGTH} characters long and contain 1 of each lower and upper case characters, and minimum 1 special character`,
@@ -24,6 +27,7 @@ export const validateForgotPassword = () => {
     body("email")
       .trim()
       .escape()
+      .notEmpty()
       .isEmail()
       .withMessage("Must be a valid email"),
   ];
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,44 +1,17 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useSearchParams } from "react-router-dom";
-import {
-  loginUser,
-  requestResetPassword,
-  verifyEmail,
-} from "../../services/auth-service.js";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth.js";
 import { useAppearance } from "../../hooks/useAppearance.js";
+import { loginUser } from "../../services/auth-service.js";
 
 export default function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [error, setError] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [showForgotPassword, setShowForgotPassword] = useState(false);
-  const [sendMail, setSendMail] = useState("");
-  const [resetMsg, setResetMsg] = useState("");
   const navigate = useNavigate();
   const { user, login } = useAuth();
   const { loadAppearance } = useAppearance();
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    const code = searchParams.get("code");
-    if (!code) return;
-    const verifyUserEmail = async () => {
-      setIsLoading(true);
-      try {
-        const data = await verifyEmail(code); // runs the verify-email endpoint
-        setSuccess(data.message);
-      } catch (err) {
-        setError(err.message); // error msg
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    verifyUserEmail();
-  }, []);
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -46,13 +19,15 @@ export default function Login() {
   }
 
   async function handleSubmit(e) {
-    e.preventDefault();
-    setError(null);
-    setIsSubmitting(true);
+    e.preventDefault(); // prevents that the page is refreshed
+    setError(null); // if fails first, this will go away when successful
+    setIsSubmitting(true); // disable the button when true
     try {
-      const result = await loginUser(formData);
-      login(result.user, result.accessToken);
-      setSuccess(`Login successful ${result.user?.username}! You will be redirected to the homepage`);
+      const result = await loginUser(formData); // tries to login the user
+      login(result.user, result.accessToken); // sets the user, and the accessToken
+      setSuccess(
+        `Login successful ${result.user?.username}! You will be redirected to the homepage`, // success msg
+      );
       setTimeout(() => {
         navigate("/"); // navigate to homepage after 3 sec
       }, 3000);
@@ -66,67 +41,41 @@ export default function Login() {
     }
   }
 
-  const sendForgotPasswordMail = async (e) => {
-    e.preventDefault();
-    setError(null);
-    setIsSubmitting(true);
-    try {
-      const data = await requestResetPassword(sendMail);
-      setResetMsg(data.message);
-    } catch (err) {
-      setError(err.message);
-    }finally{
-      setIsSubmitting(false);
-    }
-  };
-
-  if (isLoading) return <div className="login"><div className="login__card"><p className="login__status">Verifying your email...</p></div></div>;
-
   return (
     <div className="login">
       <div className="login__card">
-        {!showForgotPassword ? (
-          <form className="login__form" onSubmit={handleSubmit}>
-            <h1 className="login__title">Log In</h1>
-            <p className="login__subtitle">Welcome back to PokerDados</p>
-            {success && (
-              <p>{success}</p>
-            )}
-            <div className="login__field">
-              <label htmlFor="email">Email</label>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="you@example.com"
-                required
-              />
-            </div>
-
-            <div className="login__field">
-              <label htmlFor="password">Password</label>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder="••••••••"
-                required
-              />
-            </div>
-            <button
-              type="button"
-              className="login__forgot"
-              onClick={() => setShowForgotPassword(true)}
-            >
-              Forgot Password?
-            </button>
-
-            {error && <p className="login__error">{error}</p>}
-
+        <form className="login__form" onSubmit={handleSubmit}>
+          <h1 className="login__title">Log In</h1>
+          <p className="login__subtitle">Welcome back to PokerDados</p>
+          {success ? (
+            <span>{success}</span>
+          ) : (
+            <span className="login__error">{error}</span>
+          )}
+          <div className="login__field">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="you@example.com"
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="••••••••"
+              required
+            />
+            <Link to="/forgot-password" className="login__forgot">
+              Forgot Password
+            </Link>
             <button
               type="submit"
               className="login__submit"
@@ -134,31 +83,10 @@ export default function Login() {
             >
               {isSubmitting ? "Logging in..." : "Log In"}
             </button>
-          </form>
-        ) : (
-          <form className="login__field" onSubmit={sendForgotPasswordMail}>
-            <h2 className="login__title">Forgot Password</h2>
-            {resetMsg ? <p>{resetMsg}</p> : <p>{error}</p>}
-            <input
-              type="email"
-              value={sendMail}
-              onChange={(e) => setSendMail(e.target.value)}
-              placeholder="Enter your email.."
-            />
-            <button className="login__submit">
-              {isSubmitting ? "Sending email" : "Send reset link"}
-            </button>
-            <button
-              className="login__forgot"
-              onClick={() => setShowForgotPassword(false)}
-            >
-              Back to login
-            </button>
-          </form>
-        )}
-
+          </div>
+        </form>
         <p className="login__register">
-          Don't have an account? <Link to="/register">Register</Link>
+          Don't have an account? <Link to="/register">Register here</Link>
         </p>
       </div>
     </div>
